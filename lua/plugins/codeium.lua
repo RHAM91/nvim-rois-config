062de4e70012695f16f0275c7b52a78b63a5b5cf
Romario Torres <cs360.x@hotmail.com>
2025-12-28 17:15:42 -0600

    Plugin de Codeium

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,54 @@
+return {
+  "Exafunction/codeium.vim",
+  event = "BufEnter",
+  config = function()
+    -- Deshabilitar los atajos por defecto para evitar conflictos con blink.cmp
+    vim.g.codeium_disable_bindings = 1
+
+    -- Habilitar Codeium globalmente
+    vim.g.codeium_enabled = true
+
+    -- Control de autocompletado automático (false = manual, true = automático)
+    vim.g.codeium_manual = false
+
+    -- Keymaps personalizados para Codeium
+    -- Aceptar sugerencia completa
+    vim.keymap.set("i", "<C-g>", function()
+      return vim.fn["codeium#Accept"]()
+    end, { expr = true, silent = true, desc = "Codeium: Accept suggestion" })
+
+    -- Aceptar solo la siguiente palabra
+    vim.keymap.set("i", "<C-k>", function()
+      return vim.fn["codeium#AcceptNextWord"]()
+    end, { expr = true, silent = true, desc = "Codeium: Accept next word" })
+
+    -- Aceptar solo la siguiente línea
+    vim.keymap.set("i", "<C-l>", function()
+      return vim.fn["codeium#AcceptNextLine"]()
+    end, { expr = true, silent = true, desc = "Codeium: Accept next line" })
+
+    -- Navegar entre sugerencias
+    vim.keymap.set("i", "<C-;>", function()
+      return vim.fn["codeium#CycleCompletions"](1)
+    end, { expr = true, silent = true, desc = "Codeium: Next suggestion" })
+
+    vim.keymap.set("i", "<C-,>", function()
+      return vim.fn["codeium#CycleCompletions"](-1)
+    end, { expr = true, silent = true, desc = "Codeium: Previous suggestion" })
+
+    -- Limpiar sugerencia actual
+    vim.keymap.set("i", "<C-x>", function()
+      return vim.fn["codeium#Clear"]()
+    end, { expr = true, silent = true, desc = "Codeium: Clear suggestion" })
+
+    -- Trigger manual de completado (útil si se configura manual = true)
+    vim.keymap.set("i", "<C-Space>", function()
+      return vim.fn["codeium#Complete"]()
+    end, { expr = true, silent = true, desc = "Codeium: Trigger completion" })
+
+    -- Abrir Codeium Chat
+    vim.keymap.set("n", "<leader>cc", function()
+      vim.fn["codeium#Chat"]()
+    end, { desc = "Codeium: Open Chat" })
+  end,
+}
