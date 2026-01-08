bb975f3b87675e88bbf2160746e504e3c1e0f4b1
Romario Torres <cs360.x@hotmail.com>
2026-01-07 20:12:18 -0600

    Plugin nuevo y ajustes en atajos de letra

---
+++ lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,24 @@
+-- ========================================
+-- VIM-TMUX-NAVIGATOR
+-- ========================================
+-- Navegación sin problemas entre paneles de Neovim y tmux
+-- Usa Ctrl+h/j/k/l para navegar entre splits de Vim y paneles de tmux
+
+return {
+  'christoomey/vim-tmux-navigator',
+  lazy = false,
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
